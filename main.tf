a77b8543a776abf8ff103b343a8a4ffbb8ab5bc3
matias-ebire <matiasbire@yahoo.com>
2024-06-27 19:32:00 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,16 +13,15 @@ resource "aws_instance" "wordpress_instance" {
   }
 }
 
-resource "aws_db_instance" "wordpress_db" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
   engine               = "mysql"
-  engine_version       = "5.7"
+  engine_version       = "8.0"
   instance_class       = "db.t2.micro"
-  name                 = "wordpressdb"
-  username             = "admin"
-  password             = "mysecretpassword"
-  parameter_group_name = "default.mysql5.7"
+  username             = "foo"
+  password             = "pavodghaz"
+  parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 
   tags = {
