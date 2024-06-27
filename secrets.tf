731438408dc45dd94f9c21763265196ad4a63db7
matias-ebire <matiasbire@yahoo.com>
2024-06-27 19:18:06 -0300

    Create secrets.tf

---
+++ secrets.tf
@@ -0,0 +1,14 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+}
