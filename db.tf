b1120263b11852835afce775d8c87bace2f6fe74
matias-ebire <matiasbire@yahoo.com>
2024-06-27 22:15:51 -0300

    Create db.tf

---
+++ db.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}resource "aws_db_instance" "mydb" {
+  allocated_storage    = 20
+  identifier           = "dbinstance"
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7.30"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "shailja"
+  password             = "root12345678"
+  iam_database_authentication_enabled = true
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  publicly_accessible = true
+  tags = {
+    Name = "sqldb"
+  }
+}
